7743ca4218f443466d0e2baa5b289cf6ccd84755
amyspencerproject <amy@spencerproject.com>
2023-08-16 17:02:35 -0600

    Setup Vite React environment for Pricing Component challenge

---
+++ README.md
@@ -0,0 +1,65 @@
+# Pricing component with toggle solution
+
+This is a solution to the [Pricing component with toggle challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/pricing-component-with-toggle-8vPwRMIC).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued Development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- Control the toggle with both their mouse/trackpad and their keyboard
+- **Bonus**: Complete the challenge with just HTML and CSS
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Github Repo URL: [Pricing Component Vite Repo](https://github.com/amyspencerproject/pricing-component)
+- Live Site URL: [Pricing Component Vite Page](https://amyspencerproject.github.io/pricing-component/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Vite](https://vitejs.dev/) - React framework
+
+### What I learned
+
+- Made a checkbox toggle with a horizontal slider with CSS! Tried to make this as accessbile as possible, even used `will-change: transform;` on the toggle handle.
+- Using Vite! Trying to give up using Create React App and Vite so far has a shallow learning curve 🥳.
+- Could not use the HTML entity `&dollar;` with Vite. I had to switch to `&#36;` which is ASCII. Other HTML entities worked just fine like `&amp`; and `&times;`. I could not find a clear answer as to why.
+
+### Continued development
+
+### Useful resources
+
+- [Accessible Toggle](https://kittygiraudel.com/2021/04/05/an-accessible-toggle/) - Excellent article on how to build an accessible toggle from a checkbox.
+- [Vite documentation](https://vitejs.dev/guide/) - The documentation for Vite is very clear
+- [Setting up React on Vite ](https://www.makeuseof.com/set-up-react-app-with-vite/)
+
+## Author
+
+- Website - [Amy Spencer](https://spencerproject.com/)
+- Frontend Mentor - [@amyspencerproject](https://www.frontendmentor.io/profile/amyspencerproject)
+- Linkedin - [amyspencercodes](https://www.linkedin.com/in/amyspencercodes/)
---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <main>
+        <div className="price-components-container">
+          <h1 className="price-header">Our Pricing</h1>
+          <div className="subscription-toggle">
+            <label className="toggle" htmlFor="toggle">
+              <span className="annually">Annually</span>
+              <input
+                className="toggle-input"
+                id="toggle"
+                name="toggle"
+                type="checkbox"
+              />
+              <span className="toggle-display"></span>
+              <span className="monthly">Monthly</span>
+            </label>
+          </div>
+
+          <div className="price-cards-wrapper">
+            <div className="price-card basic">
+              <h2 className="plan-title">Basic</h2>
+              <p className="price">
+                <span className="monthly-price hide">&#36;19.99</span>
+                <span className="annual-price">&#36;199.99</span>
+              </p>
+              <ul className="plan-list">
+                <li className="plan-list-item">500 GB Storage</li>
+                <li className="plan-list-item">2 Users Allowed</li>
+                <li className="plan-list-item">Send up to 3 GB</li>
+              </ul>
+              <button className="learn-more-purple">Learn More</button>
+            </div>
+            <div className="price-card bg-purple-gradient professional">
+              <h2 className="plan-title">Professional</h2>
+              <p className="price">
+                <span className="monthly-price hide">&#36;24.99</span>
+                <span className="annual-price">&#36;249.99</span>
+              </p>
+              <ul className="plan-list">
+                <li className="plan-list-item">1 TB Storage</li>
+                <li className="plan-list-item">5 Users Allowed</li>
+                <li className="plan-list-item">Send up to 10 GB</li>
+              </ul>
+              <button className="learn-more-white">Learn More</button>
+            </div>
+            <div className="price-card master">
+              <h2 className="plan-title">Master</h2>
+              <p className="price">
+                <span className="monthly-price hide">&#36;39.99</span>
+                <span className="annual-price">&#36;399.99</span>
+              </p>
+              <ul className="plan-list">
+                <li className="plan-list-item">2 TB Storage</li>
+                <li className="plan-list-item">10 Users Allowed</li>
+                <li className="plan-list-item">Send up to 20 GB</li>
+              </ul>
+              <button className="learn-more-purple">Learn More</button>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <p className="attribution">
+          Challenge by
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          .
+        </p>
+        <p>
+          Coded by
+          <a
+            href="https://www.frontendmentor.io/profile/amyspencerproject"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Amy Spencer
+          </a>
+          .
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
